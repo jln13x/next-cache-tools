73630ed1b5bf22e1989f1773ce6199d9522e321b
jln <85513960+jln13x@users.noreply.github.com>
2025-11-16 17:21:34 +0100

    analytics

--- apps/docs/src/components/analytics.tsx
+++ apps/docs/src/components/analytics.tsx
@@ -3,12 +3,11 @@ import Script from "next/script";
 export const Analytics = () => {
   if (process.env.NODE_ENV === "development") return null;
 
-  return null;
-
-  // return (
-  //   <Script
-  //     async
-  //     src="/u/script.js"
-  //   />
-  // );
+  return (
+    <Script
+      async
+      data-website-id="6612ef9b-277d-4a37-ac1f-de7a39cc6f03"
+      src="/u/script.js"
+    />
+  );
 };
