73583e67f0e8806b6efd03db915d33195f4afd06
jln <85513960+jln13x@users.noreply.github.com>
2025-11-16 15:11:18 +0100

    replace defaults

--- apps/docs/src/lib/layout.shared.tsx
+++ apps/docs/src/lib/layout.shared.tsx
@@ -1,9 +1,10 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 export function baseOptions(): BaseLayoutProps {
   return {
+    githubUrl: "https://github.com/jln13x/next-cache-tools",
     nav: {
-      title: 'My App',
+      title: "next-cache-tools",
     },
   };
 }
