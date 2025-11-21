680c9c794cae0e60321e6c0f5c353522db102f53
jln <85513960+jln13x@users.noreply.github.com>
2025-11-21 09:05:14 +0100

    fix logo

--- packages/next-cache-tools/src/devtools/logo.tsx
+++ packages/next-cache-tools/src/devtools/logo.tsx
@@ -30,9 +30,9 @@ export function Logo() {
           width="72.21"
           height="31.147"
           filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
+          colorInterpolationFilters="sRGB"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
